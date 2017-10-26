bbfd1773e75e10ad0cc918e5b8d57968b1909004
lixanfajardo <lixan@bywave.com.au>
2017-10-26 16:02:35 +0800

    add new project

---
+++ F45TechDashboard/app/src/main/java/com/android/f45techdashboard/TestArea.java
@@ -0,0 +1,15 @@
+package com.android.f45techdashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_area);
+
+
+    }
+}
