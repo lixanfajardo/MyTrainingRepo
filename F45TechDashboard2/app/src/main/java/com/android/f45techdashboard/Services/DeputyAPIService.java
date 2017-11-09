682b485c862733890e6ccb8d4c3c60b893b01c61
lixanfajardo <lixan@bywave.com.au>
2017-11-09 17:29:57 +0800

    Added Timer Area on layouts having trouble with API calls

---
+++ F45TechDashboard2/app/src/main/java/com/android/f45techdashboard/Services/DeputyAPIService.java
@@ -0,0 +1,33 @@
+package com.android.f45techdashboard.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by LeakSun on 11/09/2017.
+ */
+
+public class DeputyAPIService extends Service{
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+
+    private String getJSONdata()
+    {
+        String deputyURL = "";
+
+
+    return null;
+    }
+
+
+
+}
---
+++ F45TechDashboard2/app/src/main/java/com/android/f45techdashboard/Services/FreshdeskAPIService.java
@@ -0,0 +1,39 @@
+package com.android.f45techdashboard.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by LeakSun on 11/09/2017.
+ */
+
+public class FreshdeskAPIService extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+
+        return null;
+    }
+
+
+ private String getJSONData()
+ {
+     String freshdeskURL = "";
+
+        return null;
+ }
+
+
+
+
+}
